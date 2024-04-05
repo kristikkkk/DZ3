6def2c156f028aecef84918125c6c64ad262827d
kristikkkk <159567318+kristikkkk@users.noreply.github.com>
2024-04-05 21:24:04 +0300

    Add files via upload

---
+++ LABA05.04.Client/Program.cs
@@ -0,0 +1,141 @@
+﻿using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+
+// создание клиента
+using TcpClient tcpClient = new TcpClient();
+await tcpClient.ConnectAsync("127.0.0.1", 8888);
+var stream = tcpClient.GetStream();
+
+using (stream)
+using (StreamReader reader = new StreamReader(stream))
+using (StreamWriter writer = new StreamWriter(stream))
+{
+    while (true)
+    {
+        string userInput = "";
+        Console.Write("Enter action (1 - get a file, 2 - save a file, 3 - delete a file): ");
+        string command = Console.ReadLine();
+        userInput += command;
+        writer.WriteLine(command);
+        writer.Flush();
+        switch (command)
+        {
+            case "1":
+                Console.Write("Do you want to get the file by name or by id (1 - name, 2 - id):");
+                string NameorId = Console.ReadLine();
+                userInput += "|" + NameorId;
+                writer.WriteLine(NameorId);
+                writer.Flush();
+                switch (NameorId)
+                {
+                    case "1":
+                        Console.Write("Enter name:  ");
+                       
+                        writer.WriteLine(Console.ReadLine());
+                        writer.Flush();
+                        Console.WriteLine("The request was sent.");
+
+                        string response1 = reader.ReadLine();
+                        if (response1 == "200")
+                        {
+                            Console.Write("The file was downloaded! Specify a name for it:  ");
+                            writer.WriteLine(Console.ReadLine()); 
+                            writer.Flush();
+                            Console.WriteLine("File saved on the hard drive!");
+                        }
+                        else if (response1 == "404")
+                        {
+                            Console.WriteLine("The response says that this file is not found!");
+                        }
+                        break;
+                    case "2": //byID
+                        Console.Write("Enter ID:  ");
+                        string serverFileID = Console.ReadLine();
+                        writer.WriteLine(serverFileID);
+                        writer.Flush();
+                        Console.WriteLine("The request was sent.");
+
+                        string response2 = reader.ReadLine();
+                        if (response2 == "200")
+                        {
+                            Console.Write("The file was downloaded! Specify a name for it:  ");
+                            writer.WriteLine(Console.ReadLine()); //userFileName
+                            writer.Flush();
+                            Console.WriteLine("File saved on the hard drive!");
+                        }
+                        else if (response2 == "404")
+                        {
+                            Console.WriteLine("The response says that this file is not found!");
+                        }
+                        break;
+                }
+                break;
+            case "2": //save a file
+                Console.Write("Enter name of the file:  ");
+                string userFileName = Console.ReadLine();
+                writer.WriteLine(userFileName);
+                writer.Flush();
+                Console.Write("Enter name of the file to be saved on server:  ");
+                string serverFileName = Console.ReadLine();
+                writer.WriteLine(serverFileName);
+                writer.Flush();
+                Console.WriteLine("The request was sent.");
+
+                string response = reader.ReadLine();
+                if (response == "200")
+                {
+                    string IdOfFile = reader.ReadLine();
+                    Console.WriteLine($"Response says that file is saved! ID = {IdOfFile}");
+                }
+                else if (response == "404")
+                {
+                    Console.WriteLine("The response says that the file can't be created");
+                }
+                break;
+            case "3": //delete
+                Console.Write("Do you want to delete the file by name or by id (1 - name, 2 - id):");
+                string NameorIdDel = Console.ReadLine();
+                writer.WriteLine(NameorIdDel);
+                writer.Flush();
+                switch (NameorIdDel)
+                {
+                    case "1": 
+                        Console.Write("Enter name:  ");
+                        writer.WriteLine(Console.ReadLine());
+                        writer.Flush();
+                        Console.WriteLine("The request was sent.");
+
+                        Console.WriteLine("The response says that this file is not found!");
+                        break;
+                    case "2": //byID
+                        Console.Write("Enter ID:  ");
+                        string serverFileID = Console.ReadLine();
+                        writer.WriteLine(serverFileID);
+                        writer.Flush();
+                        Console.WriteLine("The request was sent.");
+                        break;
+                }
+                string responseDel = reader.ReadLine();
+                if (responseDel == "200")
+                {
+                    Console.WriteLine("The response says that this file was deleted successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("The response says that this file is not found!");
+                }
+                break;
+            case "exit":
+                tcpClient.Close();
+                stream.Close();
+                reader.Close();
+                writer.Close();
+                break;
+        }
+        if (tcpClient.Connected == false) { break; }
+
+
+    }
+}
---
+++ LABA05.04.Server/Program.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var tcpListener = new TcpListener(IPAddress.Any, 8888);
+
+try
+{
+    tcpListener.Start();    // запускаем сервер
+    Console.WriteLine("Server started!");
+
+    while (true)
+    {
+        try
+        {
+            // получаем подключение в виде TcpClient
+            var tcpClient = await tcpListener.AcceptTcpClientAsync();
+            // создаем новый поток для обслуживания нового клиента
+            new Thread(async () => await ProcessClientAsync(tcpClient)).Start();
+        }
+        catch
+        {
+            break;
+        }
+
+    }
+}
+finally
+{
+    tcpListener.Stop();
+}
+// обрабатываем клиент
+async Task ProcessClientAsync(TcpClient tcpClient)
+{
+    string filePathServer = @"C:\Users\kristik\OneDrive\ynik\gittt\LABA05.04.Server";
+    string filePathClient = @"C:\Users\kristik\OneDrive\ynik\gittt\LABA05.04.Client";
+    string filePathFilesDB = @"C:\Users\kristik\OneDrive\ynik\gittt\id.txt";
+
+    SortedDictionary<int, string> serverFiles = new SortedDictionary<int, string>();
+
+    var stream = tcpClient.GetStream();
+    using (stream)
+    using (StreamReader reader = new StreamReader(stream))
+    using (StreamWriter writer = new StreamWriter(stream))
+    {
+        //заполнение словаря файлами хранящимися на сервере
+        ReadAllServerFiles(filePathFilesDB, serverFiles);
+        while (true)
+        {
+            string command = reader.ReadLine(); 
+            string serverFileName = "";
+            string userFileName = "";
+            string serverFilePath = "";
+            switch (command)
+            {
+                case "1"://get a file
+                    string nameorId = reader.ReadLine(); //считываем команду по имени либо айди
+
+                    if (nameorId == "1")// By Name
+                    {
+                        serverFileName = reader.ReadLine();
+                    }
+                    else if (nameorId == "2")// By ID
+                    {
+                        string serverFileId = reader.ReadLine();
+                        serverFileName = GetFileNameById(int.Parse(serverFileId), serverFiles);
+                        if (serverFileName == "")//значит файл не существует
+                        {
+                            writer.WriteLine("404");
+                            writer.Flush();
+                            break;
+                        }
+                    }
+                    serverFilePath = Path.Combine(filePathServer, serverFileName);//путь к файлу внутри сервера
+
+                    if (!File.Exists(serverFilePath))
+                    {
+
+                        writer.WriteLine("404");
+                        writer.Flush();
+                    }
+                    else
+                    {
+                        try
+                        {
+                            File.Copy(serverFilePath, Path.Combine(filePathClient, serverFileName));//"загружаем" файл с сервера клиенту
+                            writer.WriteLine("200");
+                            writer.Flush();
+
+                            userFileName = reader.ReadLine();
+                            if (userFileName != "")
+                            {
+                                File.Move(Path.Combine(filePathClient, serverFileName), Path.Combine(filePathClient, userFileName));//если клиент дал имя файлу то переименовываем его
+                            }
+                        }
+                        catch
+                        {
+                            writer.WriteLine("404");
+                            writer.Flush();
+                        }
+                    }
+                    break;
+                case "2": //save a file
+                    userFileName = reader.ReadLine();
+                    string userFilePath = Path.Combine(filePathClient, userFileName);
+                    serverFileName = reader.ReadLine();
+
+                    serverFilePath = Path.Combine(filePathServer, serverFileName);
+                    if (serverFileName == "" || serverFileName == "\n")
+                    {
+                        serverFilePath = Path.Combine(filePathServer, userFileName);
+
+                    }
+
+
+                    if (File.Exists(userFilePath))
+                    {
+                        File.Move(userFilePath, serverFilePath);
+                        string name = userFileName;
+                        if (!(serverFileName == ""))
+                        {
+                            File.Move(serverFilePath, Path.Combine(filePathServer, serverFileName));
+                            name = serverFileName;
+                        }
+                        int id = serverFiles.LastOrDefault().Key + 1;
+                        Console.WriteLine($"serverFiles Last Key = {serverFiles.LastOrDefault().Key}");
+                        if (serverFiles.LastOrDefault().Key == 0) { id = 1; }
+                        serverFiles.Add(id, serverFileName);
+                        //запись новой пары id-имя в файл-базу данных
+                        using (StreamWriter sw = new StreamWriter(filePathFilesDB, true)) // true указывает на необходимость добавления данных в конец файла
+                        {
+                            sw.WriteLine($"{id}|{serverFileName}");
+                        }
+                        writer.WriteLine("200");
+                        writer.Flush();
+                        writer.WriteLine(id);
+                        writer.Flush();
+
+                    }
+                    else
+                    {
+                        writer.WriteLine("404");
+                        writer.Flush();
+
+                    }
+
+                    break;
+                case "3": //delete
+                    string nameorIdDel = reader.ReadLine(); //считываем команду по имени либо айди
+
+                    if (nameorIdDel == "1")// By Name
+                    {
+                        serverFileName = reader.ReadLine();
+                    }
+                    else if (nameorIdDel == "2")// By ID
+                    {
+                        string serverFileId = reader.ReadLine();
+                        serverFileName = GetFileNameById(int.Parse(serverFileId), serverFiles);
+                        if (serverFileName == "")//значит файла нет на сервере
+                        {
+                            writer.WriteLine("404");
+                            writer.Flush();
+                            break;
+                        }
+                    }
+                    serverFilePath = Path.Combine(filePathServer, serverFileName); //папка с файлом из сервера
+
+                    if (!File.Exists(serverFilePath))
+                    {
+                        writer.WriteLine("404");
+                        writer.Flush();
+                    }
+                    else
+                    {
+                        try
+                        {
+                            File.Delete(serverFilePath);
+                            // перезаписываем файл с айди-именем тк мы файл удалили 
+                            using (StreamWriter sw = new StreamWriter(File.Open(filePathFilesDB, FileMode.Create)))
+                            {
+                                foreach (KeyValuePair<int, string> kvp in serverFiles)
+                                {
+                                    // запись пары ключ-значение, разделенной символом '|'
+                                    sw.WriteLine($"{kvp.Key}|{kvp.Value}");
+                                }
+                            }
+                            writer.WriteLine("200");
+                            writer.Flush();
+                        }
+                        catch
+                        {
+                            writer.WriteLine("404");
+                            writer.Flush();
+                        }
+                    }
+                    break;
+                case "exit":
+                    tcpClient.Close();
+                    stream.Close();
+                    reader.Close();
+                    writer.Close();
+                    tcpListener.Stop();
+                    return;
+                    break;
+
+            }
+        }
+    }
+
+    //метод добычи имени файла по айдишнику из текстового файла сервера
+    static string GetFileNameById(int fileId, SortedDictionary<int, string> serverFile)
+    {
+        foreach (var line in serverFile)
+        {
+            if (line.Key == fileId)
+            {
+                return line.Value;
+            }
+        }
+        return "";
+
+    }
+
+    //метод заполнения словаря
+    static void ReadAllServerFiles(string filePathFilesDB, SortedDictionary<int, string> serverFiles)
+    {
+        if (!File.Exists(filePathFilesDB))
+        {
+            Console.WriteLine($"Нету filePathFilesDB {filePathFilesDB}");
+            File.Create(filePathFilesDB);
+        }
+        var lines = File.ReadAllLines(filePathFilesDB);//массив строк 
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split('|');
+            serverFiles.Add(int.Parse(parts[0]), parts[1]);
+            Console.WriteLine($"{line} , {parts[0]}, {parts[1]}");
+        }
+    }
+}
